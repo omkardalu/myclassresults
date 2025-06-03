802087a7caf0e7e58bed5829f888e9c2e6af580c
desktop-ck1htmj omkar <desktop-ck1htmj.omkar@default.com>
2025-06-03 11:58:38 +0530

    make origin dynamic for both production and dev

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,6 @@
-const API_BASE = 'http://localhost:8000';
+const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:8000'  // Development
+    : window.location.origin; 
 let currentJobId = null;
 let statusInterval = null;
 
